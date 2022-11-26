ce72f68cf6c2ff177f75f8bbc043e13f88062121
Dmitriy Shishmintsev <dmitriyshishmintsev@gmail.com>
2022-11-26 13:24:58 +0300

    enter fix

--- src/Enter.js
+++ src/Enter.js
@@ -24,35 +24,42 @@ const CodeInput = styled.input`
     height: 32px;
 `
 
+const HiddenInput = styled.input`
+    opacity: 0;
+    width: 0;
+    height: 0;
+`
 class Enter extends React.Component {
     constructor(props) {
         super(props)
         this.code=""
         this.handleChange = this.handleChange.bind(this)
         this.handleBackspace = this.handleBackspace.bind(this)
+        this.sendCode = this.sendCode.bind(this)
     }
 
-    getElement(event, step, callback, err) {
+    getElement(event, step, callback) {
         let id = event.target.id.split('-')[0] + '-' + (Number(event.target.id.split('-')[1]) + step)
         let element = document.getElementById(id)
         if (element) {
             callback(element)
-        } else {
-            err()
         }
     }
 
     handleChange(event) {
         if (event.target.value != "") {
             this.code+=event.target.value
-            this.getElement(event, 1, (element) => {element.focus()}, () => {this.sendCode()})
+            this.getElement(event, 1, (element) => {element.focus()})
         }
     }
     handleBackspace(event) {
         if (event.key === 'Backspace') {
             this.code = this.code.substring(0, this.code.length-1)
             event.target.value = ""
-            this.getElement(event, -1, (element) => {element.focus()}, ()=>{})
+            this.getElement(event, -1, (element) => {
+                console.log(event.target.value)
+                element.focus()
+            }, ()=>{})
         }
     }
     sendCode() {
@@ -81,7 +88,7 @@ class Enter extends React.Component {
                         <CodeInput id="code-2" maxLength="1"/>
                         <CodeInput id="code-3" maxLength="1"/>
                         <CodeInput id="code-4" maxLength="1"/>
-
+                        <HiddenInput onFocus={this.sendCode} id="code-5" maxLength="0"/>
                     </form>
                 </EnterWrapper>    
             </Container>
